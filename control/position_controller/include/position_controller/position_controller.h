647c3a633d94aab4df8841077c86c57bb0d64ced
suresh-guttikonda <guttikondasuresh7@gmail.com>
2021-10-03 15:17:27 +0200

    Create position_controller class

---
+++ control/position_controller/include/position_controller/position_controller.h
@@ -0,0 +1,44 @@
+/**
+ *  @file   position_controller.h
+ *  @brief  rotors quadrotor position control's related functionality declaration & definition
+ *  @author neo
+ *  @data   03.10.2021
+ */
+#ifndef POSITION_CONTROLLER_POSITION_CONTROLLER_H
+#define POSITION_CONTROLLER_POSITION_CONTROLLER_H
+
+// 3rd party dependencies
+#include <Eigen/Dense>
+
+namespace position_controller {
+
+/**
+ *  @brief  PositionController class implementation.
+ *  @detail The high-level position control outputs the desired orientation, the desired collective thrust command,
+ *          the desired body rates, and the desired angular accelerations.
+ */
+class PositionController {
+ public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+        ///////////////////////////////////////////////////
+        //////////// Constructors & Destructors ///////////
+        ///////////////////////////////////////////////////
+
+    /**
+     *  @brief  PositionController's default constructor called when an instance is created.
+     */
+    PositionController();
+
+    /**
+     *  @brief  PositionController's default destructor called when an instace is destroyed.
+     */
+    ~PositionController();
+
+ protected:
+ private:
+};
+
+} /* namespace position_controller */
+
+#endif  /* POSITION_CONTROLLER_POSITION_CONTROLLER_H */
---
+++ control/position_controller/src/position_controller/position_controller.cpp
@@ -0,0 +1,7 @@
+/**
+ *  @file   position_controller.cpp
+ *  @brief  rotors quadrotor position control's related functionality implementation
+ *  @author neo
+ *  @data   03.10.2021
+ */
+#include "position_controller/position_controller.h"
